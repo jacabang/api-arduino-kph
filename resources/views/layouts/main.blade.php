23233506a2e909d58c6098ceb25ceb0a8f411617
jacabang <jeffreycabang@gmail.com>
2023-03-22 22:07:20 +0800

    fix header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -153,7 +153,7 @@
             <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
             @endif
             <a class="navbar-brand me-1 me-sm-3" href="{{URL('/')}}">
-              <div class="d-flex align-items-center"><img style="width: 125px;" class="me-2" src="{{URL('/')}}/upload/logo1.png" alt="" width="40" /><span class="font-sans-serif"></span>
+              <div class="d-flex align-items-center">HOMERGY</span>
               </div>
             </a>
             <ul class="navbar-nav align-items-center d-none d-lg-block">
