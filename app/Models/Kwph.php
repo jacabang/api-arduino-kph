a972e240579921b597666ea761911629f9eed0ac
jacabang <jeffreycabang@gmail.com>
2023-03-26 14:33:01 +0800

    Update Kwph.php

--- app/Models/Kwph.php
+++ app/Models/Kwph.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Softdeletes;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kwph extends Model
 {
